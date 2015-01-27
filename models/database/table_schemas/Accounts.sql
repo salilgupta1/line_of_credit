f411a42662127d26d361d8e92a177d36b0479a2b
salilgupta1 <salil.gupta323@gmail.com>
2015-01-27 02:13:14 -0600

    datatype changes add interest field

--- models/database/table_schemas/Accounts.sql
+++ models/database/table_schemas/Accounts.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS accounts(
 	account_id serial Primary Key,
 	user_name varchar(64) REFERENCES "users"(username)
 		ON DELETE CASCADE,
-	credit_limit NUMERIC NOT NULL,
-	apr NUMERIC NOT NULL,
-	principal NUMERIC NOT NULL,
-	date_created timestamp NOT NULL
+	credit_limit NUMERIC(8,2) NOT NULL,
+	apr NUMERIC(3,2) NOT NULL,
+	principal NUMERIC(8,2) NOT NULL DEFAULT 0.00,
+	interest NUMERIC(8,2) NOT NULL DEFAULT 0.00
 );
