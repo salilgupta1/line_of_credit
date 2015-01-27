921f986157b3e0a52172a3608158dc65d6cf2217
salilgupta1 <salil.gupta323@gmail.com>
2015-01-27 02:12:38 -0600

    datatype changes add field

--- models/database/table_schemas/Transactions.sql
+++ models/database/table_schemas/Transactions.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS transactions(
 	transaction_id serial Primary Key,
 	accountid INT REFERENCES "accounts"(account_id)
 		ON DELETE CASCADE,
-	date_of_transaction timestamp NOT NULL,
-	amount NUMERIC NOT NULL,
-	trans_type transaction_type NOT NULL 
+	transaction_day INT NOT NULL,
+	amount NUMERIC(8,2) NOT NULL,
+	trans_type transaction_type NOT NULL,
+	principal_after_trans NUMERIC(8,2) NOT NULL
 );
