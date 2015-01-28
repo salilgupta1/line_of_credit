12b7e3e6f3bdfd216dde3032b621bdc603181cc2
salilgupta1 <salil.gupta323@gmail.com>
2015-01-27 21:48:21 -0600

    triggers

--- models/database/table_schemas/Accounts.sql
+++ models/database/table_schemas/Accounts.sql
@@ -7,5 +7,21 @@ CREATE TABLE IF NOT EXISTS accounts(
 	credit_limit NUMERIC(8,2) NOT NULL,
 	apr NUMERIC(3,2) NOT NULL,
 	principal NUMERIC(8,2) NOT NULL DEFAULT 0.00,
-	interest NUMERIC(8,2) NOT NULL DEFAULT 0.00
+	interest NUMERIC(8,2) NOT NULL DEFAULT 0.00,
+
+	constraint positive_credit check (credit_limit >=0.00),
+	constraint postive_interest check (interest >=0.00)
 );
+
+CREATE TRIGGER check_principal
+	AFTER UPDATE OF principal on accounts
+	FOR EACH ROW
+	WHEN (NEW.principal < 0.00)
+	EXECUTE PROCEDURE set_principal();
+
+CREATE FUNCTION set_principal() RETURNS TRIGGER AS $zero_principal$
+    BEGIN
+    	UPDATE accounts set principal=0.0, credit_limit=new.credit_limit where account_id=new.account_id;
+        RETURN NEW;
+    END;
+$zero_principal$ LANGUAGE plpgsql;
--- models/database/table_schemas/Transactions.sql
+++ models/database/table_schemas/Transactions.sql
@@ -11,3 +11,16 @@ CREATE TABLE IF NOT EXISTS transactions(
 	trans_type transaction_type NOT NULL,
 	principal_after_trans NUMERIC(8,2) NOT NULL
 );
+
+CREATE TRIGGER principal_after_trans
+	AFTER INSERT on transactions
+	FOR EACH ROW
+	WHEN (NEW.principal_after_trans < 0.00)
+	EXECUTE PROCEDURE set_principal_after_trans();
+
+CREATE FUNCTION set_principal_after_trans() RETURNS TRIGGER AS $zero_principal$
+    BEGIN
+    	UPDATE transactions set principal_after_trans=0.0 where transaction_id=new.transaction_id;
+        RETURN NEW;
+    END;
+$zero_principal$ LANGUAGE plpgsql;
